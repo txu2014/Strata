6201bf24180138ea3937dc016d5ded828fa9db4a
Yuki Iwashita <yuki@opengamma.com>
2016-01-26 16:44:13 +0000

    trade model for cap and floor

---
+++ modules/product/src/test/java/com/opengamma/strata/product/capfloor/IborCapFloorLegTest.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2016 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.product.capfloor;
+
+import org.testng.annotations.Test;
+
+/**
+ * Test {@link IborCapFloorLeg}.
+ */
+@Test
+public class IborCapFloorLegTest {
+
+}
